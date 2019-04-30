286da1aa92334ac368ed55d8d2c049755b7de0f9
Thomas Merkel <tm@core.io>
2019-04-30 11:09:48 +0200

    Only move real data to delegate dataset

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -3,11 +3,11 @@ UUID=$(mdata-get sdc:uuid)
 DDS=zones/${UUID}/data
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
-	zfs create ${DDS}/matrix  || true
-	zfs create ${DDS}/pgsql   || true
+	zfs create ${DDS}/matrix_data || true
+	zfs create ${DDS}/pgsql       || true
 
-	if ! zfs get -o value -H mountpoint ${DDS}/matrix | grep -q /opt/matrix; then
-		zfs set mountpoint=/opt/matrix ${DDS}/matrix
+	if ! zfs get -o value -H mountpoint ${DDS}/matrix_data | grep -q /opt/matrix/data; then
+		zfs set mountpoint=/opt/matrix/data ${DDS}/matrix_data
 	fi
 	if ! zfs get -o value -H mountpoint ${DDS}/pgsql | grep -q /var/pgsql; then
 		zfs set compression=lz4 ${DDS}/pgsql
